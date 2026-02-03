925ca2a87b9af66eb55918cc7e1f3f5606f7c221
sasi <davulurisasi614@gmail.com>
2026-02-03 15:04:48 +0530

    Add files via upload

---
+++ job.sql
@@ -0,0 +1,7 @@
+create database jobportal_db;
+show databases;
+use jobportal_db;
+show tables;
+create table jobportal(jno int,jname varchar(20));
+insert into jobportal values(1,'software developer');
+select * from auth_user;
